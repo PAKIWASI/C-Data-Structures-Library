eef870e31a860ef871455bb9877504aa2f158564
PAKIWASI <ullahwasi26@gmail.com>
2025-10-09 18:40:10 +0500

    fixes

--- src/main.c
+++ src/main.c
@@ -1,19 +1,8 @@
-#include "String.h"
 #include "parse.h"
 
 
 
 int main(void)
 {
-    String str;
-    string_create_onstack(&str, "hello");
-
-    string_append_cstr(&str, "world");
-
-    string_insert_char(&str, 5, ' ');
-
-    string_print(&str);
-
-    string_destroy_fromstk(&str);
-    //return parse();
+    return parse();
 }
